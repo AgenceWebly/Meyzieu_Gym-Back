d8434148895fe83b5d8dc8f7e71672e64a182ee6
StoicFrenchDev <richard.benjamin69@yahoo.fr>
2024-06-02 10:38:10 +0200

    Removed useless imports

--- src/test/java/webly/meyzieu_gym/back/security/controller/AuthControllerTest.java
+++ src/test/java/webly/meyzieu_gym/back/security/controller/AuthControllerTest.java
@@ -8,14 +8,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,7 +30,7 @@ import java.util.Set;
 
 @WebMvcTest(AuthController.class)
 @Import(TestSecurityConfig.class)
-public class AuthControllerTest {
+class AuthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
