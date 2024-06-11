dd8527c3e65c3cf09467373b44db15472f772f50
StoicFrenchDev <richard.benjamin69@yahoo.fr>
2024-06-11 09:46:21 +0200

    Dto created

---
+++ src/main/java/webly/meyzieu_gym/back/programmanagement/repository/ProgramSeasonRepository.java
@@ -0,0 +1,8 @@
+package webly.meyzieu_gym.back.programmanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import webly.meyzieu_gym.back.programmanagement.entity.ProgramSeason;
+
+public interface ProgramSeasonRepository extends JpaRepository<ProgramSeason, Long>{
+}
